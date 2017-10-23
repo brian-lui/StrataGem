21ec4b9d943c91b2c70c416ca404dff333ee235b
garcia1000 <inaspanishway@gmail.com>
2017-10-23 22:10:05 +0800

    Finished refactoring background

--- background.lua
+++ background.lua
@@ -1,165 +1,27 @@
 local love = _G.love
 --[[
-	Draws the background, including animations.
-	Every background should have init(stage), draw(), update() methods.
+	Every background should have:
+	ID_number - used for the ordering of the backgrounds
+	thumbnail - the small picture that shows up in the selection box
+	full_pic - full sized background picture
+	init(game)
+	update(dt)
+	draw()
 --]]
 
 local common = require "class.commons"
 local image = require 'image'
 local Pic = require 'pic'
-
 local spairs = require "utilities".spairs
-
-local Background = {}
-
-function Background:init(game)
-	self.game = game
-	local stage = game.stage
-	self.backgroundParticles = {}
-	self.cloud.background_image = common.instance(Pic, game, {x = stage.x_mid, y = stage.y_mid, image = image.background.cloud.background})
-end
-
--------------------------------------------------------------------------------
------------------------------------- CLOUD ------------------------------------
--------------------------------------------------------------------------------
-
-Background.cloud = {
-	Background_ID = "Cloud",
-	big_cloud_speed = function() return 0.25 * (math.random() + 1) end,
-	med_cloud_speed = function() return 0.35 * (math.random() + 1) end,
-	small_cloud_speed = function() return 0.6 * (math.random() + 1) end,
-	daycount = 0
-}
-
-Background.cloud.Cloud = {}
-function Background.cloud.Cloud:init(background, x, y, img, speed, classification)
-  ID.background = ID.background + 1
-  self.x = x
-  self.y = y
-  self.speed_x = speed
-  self.image = img
-  self.width = img:getWidth()
-  self.height = img:getHeight()
-  self.quad = love.graphics.newQuad(0, 0, self.width, self.height, self.width, self.height)
-  self.classification = classification
-  self.ID = ID.background
-  background.backgroundParticles[ID.background] = self
-end
-
-Background.cloud.Cloud = common.class("Background.cloud.Cloud", Background.cloud.Cloud, Pic)
-
-local function Cloud_generateBigCloud(self, starting_x)
-	local x = starting_x or image.background.cloud.bigcloud1:getWidth() * -1
-	local y = math.random(0, self.game.stage.height / 4)
-	local speed = self.cloud.big_cloud_speed()
-	local img = {
-		image.background.cloud.bigcloud1,
-		image.background.cloud.bigcloud2,
-		image.background.cloud.bigcloud3,
-		image.background.cloud.bigcloud4,
-		image.background.cloud.bigcloud5,
-		image.background.cloud.bigcloud6
-	}
-	local image_index = math.random(1, #img)
-
-	common.instance(self.cloud.Cloud, self, x, y, img[image_index], speed, "Bigcloud")
-end
-
-local function Cloud_generateMedCloud(self, starting_x)
-	local x = starting_x or image.background.cloud.medcloud1:getWidth() * -1
-	local y = math.random(self.game.stage.height / 4, self.game.stage.height * 3/8)
-	local speed = self.cloud.med_cloud_speed()
-	local img = {
-		image.background.cloud.medcloud1,
-		image.background.cloud.medcloud2,
-		image.background.cloud.medcloud3,
-		image.background.cloud.medcloud4,
-		image.background.cloud.medcloud5,
-		image.background.cloud.medcloud6
-	}
-	local image_index = math.random(1, #img)
-
-	common.instance(self.cloud.Cloud, self, x, y, img[image_index], speed, "Medcloud")
-end
-
-local function Cloud_generateSmallCloud(self, starting_x)
-	local x = starting_x or image.background.cloud.smallcloud1:getWidth() * -1
-	local y = math.random(self.game.stage.height * 3/8, self.game.stage.height / 2)
-	local speed = self.cloud.small_cloud_speed()
-	local img = {
-		image.background.cloud.smallcloud1,
-		image.background.cloud.smallcloud2,
-		image.background.cloud.smallcloud3,
-		image.background.cloud.smallcloud4,
-		image.background.cloud.smallcloud5,
-		image.background.cloud.smallcloud6
-	}
-	local image_index = math.random(1, #img)
-
-	common.instance(self.cloud.Cloud, self, x, y, img[image_index], speed, "Smallcloud")
-end
-
-local function Cloud_initClouds(self)
-	local function starting_x()
-		return math.random(0, math.ceil(self.game.stage.width * 0.7))
-	end
-
-	Cloud_generateBigCloud(self, starting_x())
-	for _ = 1, 2 do
-		Cloud_generateMedCloud(self, starting_x())
-	end
-	for _ = 1, 3 do
-		Cloud_generateSmallCloud(self, starting_x())
-	end
-end
-
-function Background.cloud:drawImages()
-	self.cloud.background_image:draw()
-
-  for _, instance in spairs(self.backgroundParticles) do
-    if instance.classification == "Smallcloud" then
-      instance:draw()
-  	end
-  end
-
-  for _, instance in spairs(self.backgroundParticles) do
-    if instance.classification == "Medcloud" then
-      instance:draw()
-  	end
-  end
-
-  for _, instance in spairs(self.backgroundParticles) do
-    if instance.classification == "Bigcloud" then
-      instance:draw()
-  	end
-  end
-end
-
-function Background.cloud:update()
-	self.cloud.daycount = self.cloud.daycount + 1
-	if #self.backgroundParticles == 0 and self.cloud.daycount < 100 then
-		Cloud_initClouds()
-	end
-
-	for _, instance in pairs(self.backgroundParticles) do
-		instance.x = instance.x + instance.speed_x
-		if instance.x > self.game.stage.width + instance.width then self.backgroundParticles[instance.ID] = nil end
-	end
-
-	if self.cloud.daycount % 721 == 0 then Cloud_generateBigCloud(self) end
-	if self.cloud.daycount % 487 == 0 then Cloud_generateMedCloud(self)	end
-	if self.cloud.daycount % 361 == 0 then Cloud_generateSmallCloud(self)	end
-end
-
-function Background.cloud:reset()
-	self.backgroundParticles = {}
-	Cloud_initClouds()
-end
 
 -------------------------------------------------------------------------------
 ---------------------------- RABBIT IN A SNOWSTORM ----------------------------
 -------------------------------------------------------------------------------
-local RabbitInASnowstorm = {}
+local RabbitInASnowstorm = {
+	ID_number = 1,
+	thumbnail = image.background.colors.thumbnail,
+	full_pic = image.background.rabbitsnowstorm.background,
+}
 function RabbitInASnowstorm:init(game)
 	self.game = game
 	self.background = common.instance(Pic, game, {
@@ -178,10 +40,144 @@ function RabbitInASnowstorm:draw()
 end
 RabbitInASnowstorm = common.class("RabbitInASnowstorm", RabbitInASnowstorm)
 
+
+-------------------------------------------------------------------------------
+------------------------------------ CLOUD ------------------------------------
+-------------------------------------------------------------------------------
+local Clouds = {
+	ID_number = 2,
+	thumbnail = image.background.cloud.thumbnail,
+	full_pic = image.background.cloud.background,
+}
+function Clouds:init(game)
+	self.game = game
+	self.background = common.instance(Pic, game, {
+		x = game.stage.x_mid,
+		y = game.stage.y_mid,
+		image = image.background.cloud.background,
+	})
+	self.big_clouds, self.medium_clouds, self.small_clouds = {}, {}, {}
+	self.big_timer_func = function() return math.random(300, 400) end
+	self.medium_timer_func = function() return math.random(450, 550) end
+	self.small_timer_func = function() return math.random(700, 800) end
+	self.big_timer = math.ceil(self.big_timer_func() * 0.5)
+	self.medium_timer = math.ceil(self.medium_timer_func() * 0.5)
+	self.small_timer = math.ceil(self.small_timer_func() * 0.5)
+	self.big_cloud_images = {
+		image.background.cloud.bigcloud1,
+		image.background.cloud.bigcloud2,
+		image.background.cloud.bigcloud3,
+		image.background.cloud.bigcloud4,
+		image.background.cloud.bigcloud5,
+		image.background.cloud.bigcloud6,
+	}
+	self.medium_cloud_images = {
+		image.background.cloud.medcloud1,
+		image.background.cloud.medcloud2,
+		image.background.cloud.medcloud3,
+		image.background.cloud.medcloud4,
+		image.background.cloud.medcloud5,
+		image.background.cloud.medcloud6,
+	}
+	self.small_cloud_images = {
+		image.background.cloud.smallcloud1,
+		image.background.cloud.smallcloud2,
+		image.background.cloud.smallcloud3,
+		image.background.cloud.smallcloud4,
+		image.background.cloud.smallcloud5,
+		image.background.cloud.smallcloud6,
+	}
+	ID.background_particle = 0
+
+	self:_initClouds()
+end
+
+function Clouds:_newCloud(size, starting_x)
+	local stage = self.game.stage
+	local image, y, container, duration
+	if size == "big" then
+		image = self.big_cloud_images[math.random(1, #self.big_cloud_images)]
+		y = math.random(stage.height * 0.375, stage.height * 0.5)
+		container = self.big_clouds
+		duration = math.random(200, 280) * (starting_x and 1 - (starting_x / stage.width) or 1)
+	elseif size == "medium" then
+		image = self.medium_cloud_images[math.random(1, #self.medium_cloud_images)]
+		y = math.random(stage.height * 0.25, stage.height * 0.375)
+		container = self.medium_clouds
+		duration = math.random(300, 420) * (starting_x and 1 - (starting_x / stage.width) or 1)
+	elseif size == "small" then
+		image = self.small_cloud_images[math.random(1, #self.small_cloud_images)]
+		y = math.random(stage.height * 0.1, stage.height * 0.25)
+		container = self.small_clouds
+		duration = math.random(400, 560) * (starting_x and 1 - (starting_x / stage.width) or 1)
+	else
+		print("invalid cloud size specified")
+	end
+	x = starting_x or -image:getWidth()
+
+	local cloud = common.instance(Pic, self.game, {x = x, y = y, image = image,
+		container = container, counter = "background_particle"})
+	cloud:moveTo{duration = duration, x = stage.width + cloud.width, exit = true}
+end
+
+function Clouds:_initClouds()
+	local init_clouds = {"small", "small", "small", "medium", "medium", "big"}
+	for _, size in pairs(init_clouds) do
+		self:_newCloud(size, math.random(0, self.game.stage.width * 0.7))
+	end
+
+end
+
+function Clouds:update(dt)
+	for _, v in pairs(self.big_clouds) do v:update(dt) end
+	for _, v in pairs(self.medium_clouds) do v:update(dt) end
+	for _, v in pairs(self.small_clouds) do v:update(dt) end
+
+	if self.big_timer <= 0 then
+		self:_newCloud("big")
+		self.big_timer = self.big_timer_func()
+	else
+		self.big_timer = self.big_timer - 1
+	end
+
+	if self.medium_timer <= 0 then
+		self:_newCloud("medium")
+		self.medium_timer = self.medium_timer_func()
+	else
+		self.medium_timer = self.medium_timer - 1
+	end
+
+	if self.small_timer <= 0 then
+		self:_newCloud("small")
+		self.small_timer = self.small_timer_func()
+		local c = 0
+		for _ in pairs(self.small_clouds) do c = c + 1 end
+		print("self.small_clouds", c)
+
+	else
+		self.small_timer = self.small_timer - 1
+	end
+end
+
+function Clouds:draw()
+	self.background:draw()
+	for _, v in spairs(self.small_clouds) do v:draw() end
+	for _, v in spairs(self.medium_clouds) do v:draw() end
+	for _, v in spairs(self.big_clouds) do v:draw() end
+end
+
+Clouds = common.class("Clouds", Clouds)
+
+
+
 -------------------------------------------------------------------------------
 ---------------------------------- STARFALL -----------------------------------
 -------------------------------------------------------------------------------
-local Starfall = {}
+local Starfall = {
+	ID_number = 3,
+	thumbnail = image.background.starfall.thumbnail,
+	full_pic = image.background.starfall.background,
+}
 function Starfall:init(game)
 	self.game = game
 	self.background = common.instance(Pic, game, {
@@ -232,7 +228,7 @@ end
 
 function Starfall:draw()
 	self.background:draw()
-	for _, v in pairs(self.stars) do v:draw() end
+	for _, v in spairs(self.stars) do v:draw() end
 end
 Starfall = common.class("Starfall", Starfall)
 
@@ -240,14 +236,25 @@ Starfall = common.class("Starfall", Starfall)
 -------------------------------------------------------------------------------
 ----------------------------------- COLORS -----------------------------------
 -------------------------------------------------------------------------------
-local Colors = {}
+local Colors = {
+	ID_number = 4,
+	thumbnail = image.background.colors.thumbnail,
+	full_pic = image.background.colors.white,
+}
 function Colors:init(game)
 	self.game = game
 	self.t = 0
-	self.colors = {} -- container for stars
 	self.current_color = common.instance(Pic, self.game, 
 		{x = game.stage.x_mid, y = game.stage.y_mid, image = image.background.colors.white})
 	self.previous_color = nil
+	self.timing_full_cycle = 1800
+	self.timings = {
+		[0] = image.background.colors.white,
+		[360] = image.background.colors.blue,
+		[720] = image.background.colors.red,
+		[1080] = image.background.colors.green,
+		[1440] =  image.background.colors.yellow,
+	}
 	ID.background_particle = 0
 end
 
@@ -262,21 +269,11 @@ function Colors:_newColor(image)
 end
 
 function Colors:update(dt)
-	self.t = (self.t + 1) % 1800
+	self.t = (self.t + 1) % self.timing_full_cycle
 	self.current_color:update(dt)
 	if self.previous_color then self.previous_color:update(dt) end
-
-	if self.t % 1800 == 360 then
-		self:_newColor(image.background.colors.blue)
-	elseif self.t % 1800 == 720 then
-		self:_newColor(image.background.colors.red)
-	elseif self.t % 1800 == 1080 then
-		self:_newColor(image.background.colors.green)
-	elseif self.t % 1800 == 1440 then
-		self:_newColor(image.background.colors.yellow)
-	elseif self.t % 1800 == 0 then
-		self:_newColor(image.background.colors.white)
-	end
+	local new = self.timings[self.t]
+	if new then self:_newColor(new) end
 end
 
 function Colors:draw()
@@ -285,18 +282,20 @@ function Colors:draw()
 end
 Colors = common.class("Colors", Colors)
 
--- Change this to key = name, value = table later
-Background.list = {
-	{background = "RabbitInASnowstorm", thumbnail = image.background.colors.thumbnail, full = image.background.colors.white},
-	{background = "Colors", thumbnail = image.background.colors.thumbnail, full = image.background.colors.white},
-	{background = Background.cloud, thumbnail = image.background.cloud.thumbnail, full = image.background.cloud.background},
-	{background = "Starfall", thumbnail = image.background.starfall.thumbnail, full = image.background.starfall.background},
-}
-
---]]
-
+local Background = {}
+Background.Clouds = Clouds
 Background.RabbitInASnowstorm = RabbitInASnowstorm
 Background.Starfall = Starfall
 Background.Colors = Colors
 
+-- we need this for the low quality charselect code. Eventually we should delete this
+local list = {}
+local function compare_func(a, b)
+	return Background[a].ID_number < Background[b].ID_number
+end
+
+for k, v in spairs(Background, compare_func) do
+	list[v.ID_number] = {background = k, thumbnail = v.thumbnail, full = v.full_pic}
+end
+Background.list = list
 return common.class("Background", Background)
