da20880ede45a983e94e5b1656d2e0689d12c173
garcia1000 <inaspanishway@gmail.com>
2017-10-23 23:06:07 +0800

    Just some more changes

--- gs_title.lua
+++ gs_title.lua
@@ -1,15 +1,9 @@
-local love = _G.love
-
 local common = require "class.commons"
 local image = require 'image'
 local Pic = require 'pic'
 local tween = require 'tween'
 
 local title = {}
-
-function title:init()
-	self.timeStep, self.timeBucket = 1/60, 0
-end
 
 --[[ create a clickable object
 	mandatory parameters: name, image, image_pushed, end_x, end_y
@@ -25,21 +19,17 @@ function title:_createButton(params)
 		y = params.start_y or params.end_y,
 		transparency = params.start_transparency or 255,
 		image = params.image,
-		container = self.ui_clickable,
+		container = title.ui_clickable,
 		counter = "background_particle",
 	})
 	button:moveTo{duration = params.duration, x = params.end_x, y = params.end_y,
 		transparency = params.end_transparency or 255,
 		easing = params.easing or "linear", exit = params.exit}
-
 	button.pushed = function()
 		self.sound:newSFX("button")
 		button:newImage(params.image_pushed)
 	end
-	button.released = function()
-		button:newImage(params.image)
-	end
-
+	button.released = function() button:newImage(params.image) end
 	button.action = params.action
 end
 
@@ -56,21 +46,20 @@ function title:_createImage(params)
 		y = params.start_y or params.end_y,
 		transparency = params.transparency or 255,
 		image = params.image,
-		container = self.ui_static,
+		container = title.ui_static,
 		counter = "background_particle",
 	})
 	button:moveTo{duration = params.duration, x = params.end_x, y = params.end_y,
 		transparency = params.transparency, easing = params.easing, exit = params.exit}
 end
 
-function title:enter()
-	local stage = self.stage
-	self.clicked = nil
-	if self.sound:getCurrentBGM() ~= "bgm_menu" then self.sound:stopBGM() end
-	self.current_background = common.instance(self.background.RabbitInASnowstorm, self)
-	self.ui_clickable = {}
-	self.ui_static = {}
-
+-- After the initial tween, we keep the icons here if returning to title screen
+-- So we put it in init(), not enter() like in the other states
+function title:init()
+	local stage = self.stage	
+	self.timeStep, self.timeBucket = 1/60, 0
+	title.ui_clickable = {}
+	title.ui_static = {}
 	title._createButton(self, {
 		name = "vscpu",
 		image = image.title.vscpu,
@@ -85,7 +74,6 @@ function title:enter()
 			self.statemanager:switch(require "gs_charselect")
 		end,
 	})
-
 	title._createButton(self, {
 		name = "online",
 		image = image.title.online,
@@ -100,7 +88,6 @@ function title:enter()
 			self.statemanager:switch(require "gs_lobby") self.client:connect()
 		end,
 	})
-
 	title._createImage(self, {
 		name = "logo",
 		image = image.title.logo,
@@ -115,49 +102,55 @@ function title:enter()
 				self.sound:newBGM("bgm_menu", true)
 			end
 		end},
-	})
+	})	
+end
+
+function title:enter()
+	title.clicked = nil
+	if self.sound:getCurrentBGM() ~= "bgm_menu" then self.sound:stopBGM() end
+	title.current_background = common.instance(self.background.RabbitInASnowstorm, self)
 end
 
 function title:update(dt)
-	self.current_background:update(dt)
-	for _, v in pairs(self.ui_clickable) do v:update(dt) end
-	for _, v in pairs(self.ui_static) do v:update(dt) end
+	title.current_background:update(dt)
+	for _, v in pairs(title.ui_clickable) do v:update(dt) end
+	for _, v in pairs(title.ui_static) do v:update(dt) end
 end
 
 function title:draw()
-	self.current_background:draw()
-	for _, v in pairs(self.ui_static) do v:draw() end
-	for _, v in pairs(self.ui_clickable) do v:draw() end
+	title.current_background:draw()
+	for _, v in pairs(title.ui_static) do v:draw() end
+	for _, v in pairs(title.ui_clickable) do v:draw() end
 end
 
 local pointIsInRect = require "utilities".pointIsInRect
 function title:mousepressed(x, y)
-	for _, button in pairs(self.ui_clickable) do
+	for _, button in pairs(title.ui_clickable) do
 		if pointIsInRect(x, y, button:getRect()) then
-			self.clicked = button
+			title.clicked = button
 			button.pushed()
 			return
 		end
 	end
-	self.clicked = false
+	title.clicked = false
 end
 
 function title:mousereleased(x, y)
-	for _, button in pairs(self.ui_clickable) do
+	for _, button in pairs(title.ui_clickable) do
 		button.released()
-		if pointIsInRect(x, y, button:getRect()) and self.clicked == button then
+		if pointIsInRect(x, y, button:getRect()) and title.clicked == button then
 			button.action()
 			break
 		end
 	end
-	self.clicked = false
+	title.clicked = false
 end
 
 function title:mousemoved(x, y)
-	if self.clicked then
-		if not pointIsInRect(x, y, self.clicked:getRect()) then
-			self.clicked.released()
-			self.clicked = false
+	if title.clicked then
+		if not pointIsInRect(x, y, title.clicked:getRect()) then
+			title.clicked.released()
+			title.clicked = false
 		end
 	end
 end
