0ae5dff7fbf831f800d9a376ea6fb3a4f432549d
Nathaniel Musgrove <ProtonZero@users.noreply.github.com>
2017-11-26 02:20:34 -0800

    Basic tlfres implementation
    Most scaling and letterboxing is correct. Gems are invisible most of the time.

--- main.lua
+++ main.lua
@@ -8,27 +8,28 @@ local maxWindowWidth
 local maxWindowHeight
 
 function love.load()
-	love.window.setMode(drawspace.width * drawspace.scale, drawspace.height * drawspace.scale, {resizable=true})
 	love.window.setTitle("StrataGem!")
 	game = common.instance(require "game")
-	
+
 	-- Using the largest full screen dimension as the upper bound for window size
 	fullscreenDimensions = love.window.getFullscreenModes()
 	table.sort(fullscreenDimensions, function(a, b) return a.width*a.height > b.width*b.height end)
 	maxWindowWidth, maxWindowHeight = fullscreenDimensions[1].width, fullscreenDimensions[1].height
+
+	-- default windowed resolution is half native
+	local desktopWidth, desktopHeight = love.window.getDesktopDimensions()
+	love.window.setMode(desktopWidth / 2, desktopHeight / 2)
 end
 -- local sandbox = require 'animationsandbox'
 
-local __NOP__ = function () end 
+local __NOP__ = function () end
 
 function love.draw()
-	local f = game.draw or __NOP__
-	love.graphics.push("all")
-		love.graphics.scale(drawspace.scale, drawspace.scale)
-		f(game)
-	drawspace.tlfres.beginRendering(drawspace.width, drawspace.height)
-	drawspace.tlfres.endRendering({0, 0, 0, 0}) -- Using an opaque color so we don't have a visible letterbox
-	love.graphics.pop()
+	if game.draw then
+		drawspace.tlfres.beginRendering(drawspace.width, drawspace.height)
+		game:draw()
+		drawspace.tlfres.endRendering({0, 0, 0, 0}) -- Using an opaque color so we don't have a visible letterbox
+	end
 end
 
 function love.update(dt)
@@ -40,26 +41,26 @@ function love.keypressed(key)
 end
 
 function love.mousepressed(x, y, button, istouch)
-	x, y = x / drawspace.scale, y / drawspace.scale
+	x, y = drawspace.tlfres.getMousePosition(drawspace.width, drawspace.height)
 	local f = game.mousepressed or __NOP__
 	f(game, x, y, button, istouch)
 end
 
 function love.mousereleased(x, y, button, istouch)
-	x, y = x / drawspace.scale, y / drawspace.scale
+	x, y = drawspace.tlfres.getMousePosition(drawspace.width, drawspace.height)
 	local f = game.mousereleased or __NOP__
 	f(game, x, y, button, istouch)
 end
 
 function love.mousemoved(x, y, dx, dy)
-	x, y, dx, dy = x / drawspace.scale, y / drawspace.scale, dx / drawspace.scale, dy / drawspace.scale
-	local f = game.mousemoved or __NOP__
-	f(game, x, y, dx, dy)
+	if game.mousemoved then
+		x, y = drawspace.tlfres.getMousePosition(drawspace.width, drawspace.height)
+		local scale = drawspace.tlfres.getScale(drawspace.width, drawspace.height)
+		dx, dy = dx / scale, dy / scale
+		game:mousemoved(x, y, dx, dy)
+	end
 end
 
 function love.resize(w, h)
-	local newHeight = math.min(maxWindowHeight, h)
-	local newWidth = math.min(maxWindowWidth, newHeight * drawspace.aspectRatio)
-	love.window.setMode(newWidth, newHeight, {resizable = true})
 	drawspace.scale = drawspace.tlfres.getScale(drawspace.width, drawspace.height) -- Recalculate scale based on new window dimensions
 end
