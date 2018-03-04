8ba7b0af9e4d3f47fd9e22b4b86e834bbeb0d22d
garcia1000 <inaspanishway@gmail.com>
2018-03-04 17:17:10 +0800

    Wolfgang notes

--- characters/wolfgang.lua
+++ characters/wolfgang.lua
@@ -1,9 +1,37 @@
-local love = _G.love
+--[[ Color: blue
+Passive: Wolfgang has a BARK meter. Every time you make a match of a certain color,
+the BARK meter gains a letter. (Blue, Amarillo, Red, Kreen). When the Bark meter
+is filled, your next gem cluster you gain will contain a Dog piece. Dogs placed
+in your basin are good dogs. Good dogs are wild and last until matched. Basins
+placed in the opponent's basin (rush) are bad dogs. Bad dogs do not listen and
+do nothing. They last for 3 turns and then go home.
 
+Passive animation:
+BARK meter (unlit) appears below the super meter. Whenever a match happens, the
+appropriate letter lights up with the usual sort of dust explosion. (similar to
+the dust explosion when you double cast.) Also, when the match happens, the
+appropriate words (BLUE! AMARILLO! etc) associated with the color appear at the
+location of the match, slightly rotated (from -20 to 20 degrees) and float up in
+the air like Final Fantasy damage numbers. (float up about 78 pixels, decelerating,
+linger for .5 second once they reach the final location, and then fade out).
+
+When the BARK meter is lit entirely, a good dog piece (Random) appears in the next
+set of gems on the stars when it moves again.
+
+When you drag a piece to the opponent's side (rush), the good dog should change to
+a bad dog as soon as you hover to the other side, and return to good dog if you
+bring the piece back.
+
+Super: The bottom most platform in your hand gains a double Dog (or becomes a
+double dog), and the next 4 clusters that come through your conveyor belt also
+contain dogs. Wolfgang still plays a gem this turn.
+--]]
+
+local love = _G.love
 local common = require "class.commons"
 local Character = require "character"
 local image = require 'image'
-
+local Pic = require 'pic'
 local Wolfgang = {}
 
 Wolfgang.full_size_image = love.graphics.newImage('images/portraits/wolfgang.png')
