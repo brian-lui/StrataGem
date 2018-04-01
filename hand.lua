041470ff93ef5c1ff30590c1da4e0c6453a49add
garcia1000 <inaspanishway@gmail.com>
2018-04-01 22:58:56 +0800

    Demo mode toggle

--- hand.lua
+++ hand.lua
@@ -302,7 +302,7 @@ end
 -- Update function only called at end of turn
 function Hand:endOfTurnUpdate()
 	for i = 1, 5 do
-		assert(self[i].platform, "There was no platform in hand position " .. i .. "!! Please report bug. Platforms: ")
+		assert(self[i].platform, "No platform in hand position " .. i .. "!! PLEASE SEND SCREENSHOT TO CODER.")
 		self[i].platform:setFastSpin(false)
 	end
 	self.damage = self.damage + 4
