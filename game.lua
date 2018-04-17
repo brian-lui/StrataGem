798eb4f5c0259e75d4bca378b6853a2d8a1d7743
garcia1000 <inaspanishway@gmail.com>
2018-04-17 21:52:34 +0800

    Debug printout now shows the current pause phase, not the next pause phase

--- game.lua
+++ game.lua
@@ -60,7 +60,7 @@ function Game:init()
 	self.debug_drawGrid = true
 	self.debug_overlay = function()
 		if self.current_phase == "Pause" then
-			return "Pause " .. self.phase.frames_until_next_phase .. " (" .. self.phase.next_phase .. ")"
+			return "Pausing at " .. self.phase.current_phase_for_debug_purposes_only .. ", " .. self.phase.frames_until_next_phase .. "\nGarbage this round: " .. self.phase.garbage_this_round
 		else
 			return self.current_phase
 		end
