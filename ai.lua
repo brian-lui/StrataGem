4bec2659361736aa6bf67310424a42849407c52b
garcia1000 <inaspanishway@gmail.com>
2018-04-17 21:52:03 +0800

    CPU now activates super the turn it is full

--- ai.lua
+++ ai.lua
@@ -88,6 +88,11 @@ local function placePiece(self, piece, coords, place_type)
 	self:queueAction(piece.dropIntoBasin, {piece, coords})
 end
 
+local function playSuper(self, super_params)
+	super_params = super_params or {}
+	self:queueAction(function() self.player.supering = true end, super_params)
+end
+
 -- returns a scoring for all possible pieces and their placements
 local function generateScoreMatrices(grid, player)
 	local piece_list = enumeratePieces(player)
@@ -125,50 +130,55 @@ end
 function ai:evaluateActions()
 	local player = self.player
 	if self.countdown == 0 then
-		-- Get a set of moves that yield the highest score
-		local matrices = generateScoreMatrices(self.game.grid, player)
-		local maximum_score = 0
-		local possible_moves = {}
-		for rot = 1, 4 do
-			local h_adj = (rot+1) % 2
-			for pc = 1, #matrices[rot] do
-				for col = player.start_col, player.end_col - h_adj do
-					local score = matrices[rot][pc][col]
-					if score > maximum_score then	-- Make a fresh table
-						maximum_score = score
-						possible_moves = {{rotation = rot, piece_idx = pc, column = col}}
-					elseif score == maximum_score then	-- Add to the current table
-						possible_moves[#possible_moves+1] = {rotation = rot, piece_idx = pc, column = col}
+		-- play super if available, using params
+		if player:canUseSuper() then
+			local super_params = nil -- TODO
+			playSuper(self, super_params)
+		else
+			-- Get a set of moves that yield the highest score
+			local matrices = generateScoreMatrices(self.game.grid, player)
+			local maximum_score = 0
+			local possible_moves = {}
+			for rot = 1, 4 do
+				local h_adj = (rot+1) % 2
+				for pc = 1, #matrices[rot] do
+					for col = player.start_col, player.end_col - h_adj do
+						local score = matrices[rot][pc][col]
+						if score > maximum_score then	-- Make a fresh table
+							maximum_score = score
+							possible_moves = {{rotation = rot, piece_idx = pc, column = col}}
+						elseif score == maximum_score then	-- Add to the current table
+							possible_moves[#possible_moves+1] = {rotation = rot, piece_idx = pc, column = col}
+						end
 					end
 				end
 			end
-		end
 
-		if maximum_score > 0 then
-			local selected = possible_moves[math.random(#possible_moves)]
-			--local selected = possible_moves[1] -- for debug, always select first piece
-			local piece = enumeratePieces(player)[selected.piece_idx]
-			for _ = 1, selected.rotation do
-				piece:rotate()
+			if maximum_score > 0 then
+				local selected = possible_moves[math.random(#possible_moves)]
+				--local selected = possible_moves[1] -- for debug, always select first piece
+				local piece = enumeratePieces(player)[selected.piece_idx]
+				for _ = 1, selected.rotation do
+					piece:rotate()
+				end
+
+				placePiece(self, piece, getCoords(piece, selected.column))
+			elseif player.cur_burst >= player.RUSH_COST and
+			self.game.grid:getFirstEmptyRow(1) >= self.game.grid.RUSH_ROW then
+				local piece = selectRandomPiece(player)
+				if piece.is_horizontal then	-- Always do vertical rushes.
+					piece:rotate()
+				end
+
+				placePiece(self, piece, {player.enemy.start_col, player.enemy.start_col}, "rush")
+			else
+				-- random play
+				local piece = selectRandomPiece(player)
+				local coords = getCoords(piece, selectRandomColumn(piece, player))
+
+				placePiece(self, piece, coords)
 			end
-
-			placePiece(self, piece, getCoords(piece, selected.column))
-		elseif player.cur_burst >= player.RUSH_COST and
-		self.game.grid:getFirstEmptyRow(1) >= self.game.grid.RUSH_ROW then
-			local piece = selectRandomPiece(player)
-			if piece.is_horizontal then	-- Always do vertical rushes.
-				piece:rotate()
-			end
-
-			placePiece(self, piece, {player.enemy.start_col, player.enemy.start_col}, "rush")
-		else
-			-- random play
-			local piece = selectRandomPiece(player)
-			local coords = getCoords(piece, selectRandomColumn(piece, player))
-
-			placePiece(self, piece, coords)
 		end
-
 		self.countdown = 5
 		self.finished = true
 	else
