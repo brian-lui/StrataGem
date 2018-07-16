c708f019b905bc06dda530e1e30e2014959ec911
Brian Lui <inaspanishway@gmail.com>
2018-07-16 22:10:15 +0800

    Renamed inits.FONT CARTOON fonts and row adjustments

--- helpers/inits.lua
+++ helpers/inits.lua
@@ -32,9 +32,12 @@ inits.FONT = {
 	STANDARD_REGULAR = love.graphics.newFont('/fonts/anonymous.ttf', 20),
 	STANDARD_MEDIUM = love.graphics.newFont('/fonts/anonymous.ttf', 30),
 	STANDARD_BIGGER = love.graphics.newFont('/fonts/anonymous.ttf', 40),
-	CARTOON_REGULAR = love.graphics.newFont('/fonts/BD_Cartoon_Shout.ttf', 20),
-	CARTOON_MEDIUM = love.graphics.newFont('/fonts/BD_Cartoon_Shout.ttf', 30),
-	CARTOON_BIGGER = love.graphics.newFont('/fonts/BD_Cartoon_Shout.ttf', 40),
+	CARTOON_SMALL = love.graphics.newFont('/fonts/BD_Cartoon_Shout.ttf', 30),
+	CARTOON_MEDIUM = love.graphics.newFont('/fonts/BD_Cartoon_Shout.ttf', 60),
+	CARTOON_BIG = love.graphics.newFont('/fonts/BD_Cartoon_Shout.ttf', 90),
 }
+inits.FONT.CARTOON_SMALL_ROWADJUST = -math.ceil(inits.FONT.CARTOON_SMALL:getHeight() * 0.5)
+inits.FONT.CARTOON_MEDIUM_ROWADJUST = -math.ceil(inits.FONT.CARTOON_MEDIUM:getHeight() * 0.5)
+inits.FONT.CARTOON_BIG_ROWADJUST = -math.ceil(inits.FONT.CARTOON_BIG:getHeight() * 0.5)
 
 return inits
