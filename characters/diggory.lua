6c0a3fb8a5b1a23ca4aeccab9514f546eadfb992
Brian Lui <inaspanishway@gmail.com>
2018-07-20 22:41:34 +0800

    New: added passive ability dust clouds

--- characters/diggory.lua
+++ characters/diggory.lua
@@ -32,7 +32,7 @@ Diggory.super_images = {
 	empty = images.ui_super_empty_yellow,
 	full = images.ui_super_full_yellow,
 	glow = images.ui_super_glow_yellow,
-	overlay = love.graphics.newImage('images/dummy.png'),
+	overlay = love.graphics.newImage('images/characters/diggory/diggorylogo.png'),
 }
 Diggory.burst_images = {
 	partial = images.ui_burst_part_yellow,
@@ -40,9 +40,85 @@ Diggory.burst_images = {
 	glow = {images.ui_burst_partglow_yellow, images.ui_burst_fullglow_yellow}
 }
 
+Diggory.special_images = {
+	clod = {
+		love.graphics.newImage('images/characters/diggory/clod1.png'),
+		love.graphics.newImage('images/characters/diggory/clod2.png'),
+		love.graphics.newImage('images/characters/diggory/clod3.png'),
+		love.graphics.newImage('images/characters/diggory/clod4.png'),
+	},
+	dustcloud = love.graphics.newImage('images/characters/diggory/dustcloud.png'),
+}
+
 Diggory.sounds = {
 	bgm = "bgm_diggory",
 }
+
+-------------------------------------------------------------------------------
+-- these appear when passive gets activated
+local PassiveClouds = {}
+function PassiveClouds:init(manager, tbl)
+	Pic.init(self, manager.game, tbl)
+	local counter = self.game.inits.ID.particle
+	manager.allParticles.CharEffects[counter] = self
+	self.manager = manager
+end
+
+function PassiveClouds:remove()
+	self.manager.allParticles.CharEffects[self.ID] = nil
+end
+
+function PassiveClouds.generate(game, owner, x, y)
+	local image = owner.special_images.dustcloud
+	local smokes = {
+		left = {
+			sign = -1,
+			flip_x = math.random() < 0.5,
+			flip_y = math.random() < 0.5,
+		},
+		right = {
+			sign = 1,
+			flip_x = math.random() < 0.5,
+			flip_y = math.random() < 0.5,
+		},
+	}
+
+	for _, smoke in pairs(smokes) do
+		local p = common.instance(PassiveClouds, game.particles, {
+			x = x,
+			y = y,
+			image = image,
+			draw_order = 4,
+			h_flip = smoke.flip_x,
+			v_flip = smoke.flip_y,
+			owner = owner,
+		})
+		p.scaling = 0.5
+		p:change{
+			duration = 30,
+			x = x + game.stage.width * 0.05 * smoke.sign,
+			y = y - game.stage.height * 0.02,
+			rotation = smoke.sign,
+			scaling = 0.8,
+			easing = "outQuart",
+		}
+		p:change{
+			duration = 30,
+			rotation = 1.25 * smoke.sign,
+			scaling = 1,
+			transparency = 0,
+			remove = true,
+		}
+	end
+end
+
+PassiveClouds = common.class("PassiveClouds", PassiveClouds, Pic)
+
+-------------------------------------------------------------------------------
+Diggory.fx = {
+	passive_clouds = PassiveClouds,
+}
+-------------------------------------------------------------------------------
 
 function Diggory:init(...)
 	Character.init(self, ...)
@@ -110,12 +186,20 @@ function Diggory:afterGravity()
 	local delay = 0
 	local go_to_gravity
 
+	-- activate passive
 	if not self.slammed_this_turn then
 		for _, gem in pairs(self.slammy_gems) do
 			local below_gem = grid[gem.row + 1][gem.column].gem
 			if below_gem then
 				if below_gem.color ~= "yellow" then
-					local time_to_explode = grid:destroyGem{
+					-- clouds animation
+					self.fx.passive_clouds.generate(
+						game,
+						self,
+						below_gem.x,
+						below_gem.y
+					)
+
 					local time_to_explode, particle_duration = grid:destroyGem{
 						gem = below_gem,
 						credit_to = self.player_num,
