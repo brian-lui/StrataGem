b380c1cfb38e0282ede91c8c415f172c4c666ff4
Brian <inaspanishway@gmail.com>
2020-08-03 21:36:13 +1000

    New: gem.contained_items for character object interactions

--- gem.lua
+++ gem.lua
@@ -43,6 +43,7 @@ function Gem:init(params)
 	self.exploding_gem_image = params.exploding_gem_image
 	self.grey_exploding_gem_image = params.grey_exploding_gem_image
 	self.pop_particle_image = params.pop_particle_image
+	self.contained_items = {} -- displayed objects from character abilities
 end
 
 -- If a non-standard gem color is created, must also provide exploding gem and
