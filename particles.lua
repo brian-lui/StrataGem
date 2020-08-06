0e4f5f7c520f23d69855676a8cc31710c589a529
Brian <inaspanishway@gmail.com>
2020-08-06 21:04:27 +1000

    New: PlacedGem will  now show contained items

--- particles.lua
+++ particles.lua
@@ -1654,15 +1654,7 @@ end
 -- draw anything that's contained by the gem, too
 function PlacedGem:draw()
 	Pic.draw(self)
-
---[[
-	TODO: check if this is ok. Sometimes it's wrong when rotated?
-	for _, obj in pairs(self.gem.contained_items) do
-		if obj.draw then
-			obj:draw{x = self.x, y = self.y}
-		end
-	end
---]]
+	self.gem:drawContainedItems{x = self.x, y = self.y}
 end
 
 PlacedGem = common.class("PlacedGem", PlacedGem, Pic)
